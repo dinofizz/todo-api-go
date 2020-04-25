846cc2b10b8b1d58d4131ccb70e9427c06ead9c5
Dino Fizzotti <dino@dinofizzotti.com>
2020-04-25 17:38:03 +0100

    Working multi-arch Docker build

--- gobuild_multi_arch.sh
+++ gobuild_multi_arch.sh
@@ -11,20 +11,20 @@ contains() {
     fi
 }
 
-echo "$(pwd)" > /log
 contains $1 "amd64"
-echo "$(ls -al)" >> /log
 if [ $? -eq 0 ]
 then
   go build -o todo-api -v
-  return 0
+  exit
 fi
 
 contains $1 "arm/v7"
 if [ $? -eq 0 ]
 then
-  env CC=arm-linux-gnueabi-gcc CGO_ENABLED=1 GOOS=linux GOARCH=arm GOARM=7 go build -o todo-api -v
-  return 0
+  env CC=arm-linux-gnueabihf-gcc CXX=arm-linux-gnueabihf-g++ \
+      CGO_ENABLED=1 GOOS=linux GOARCH=arm GOARM=7 \
+      go build -v -o todo-api
+  exit
 fi
 
 exit 1
